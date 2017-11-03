f6a9148cc5bf008e470bc00ed7c15a9083b7d8f9
EchoXiaoLang <1198673643@qq.com>
2017-11-03 16:13:28 +0800

    ceshi

--- conf/default.py
+++ conf/default.py
@@ -22,10 +22,10 @@ from django.conf.global_settings import *  # noqa
 # 应用基本信息配置 (请按照说明修改)
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
-APP_ID = ''
-APP_TOKEN = ''
+APP_ID = 'testapp'
+APP_TOKEN = '0a8a37cf-31c4-4bff-a200-706c59d743a6'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
-BK_PAAS_HOST = ''
+BK_PAAS_HOST = 'http://paas-dev.tjbk.tongji.edu.cn:80'
 
 # 是否启用celery任务
 IS_USE_CELERY = True
