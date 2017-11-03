44cbd7bafdbc535d2b7929ce192a5f17872f48d1
EchoXiaoLang <1198673643@qq.com>
2017-11-03 16:33:16 +0800

    ceshi

--- home_application/views.py
+++ home_application/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from common.mymako import render_mako_context
-
+from   django.http import HttpResponse
 
 def home(request):
     """
@@ -22,3 +22,7 @@ def contactus(request):
     联系我们
     """
     return render_mako_context(request, '/home_application/contact.html')
+
+def index(request):
+
+    return HttpResponse("hello world");
